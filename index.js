b8f1450437e7ffe79e6ec253b998d2e69eec349a
Valentin Gallardo <valengallardo2@gmail.com>
2021-05-30 12:05:04 -0300

    Agregado SendFile para home.html

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000");
 })
 
+app.get("/", function(req, res){
+    let htmlPath = path.join(__dirname, "/views/home.html");
+    res.sendFile(htmlPath);
+})
